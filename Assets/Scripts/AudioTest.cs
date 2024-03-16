061e0b547f69c61779d7f1b3a36efac50db11f05
Dinçer Can Elitok <dincercanelitok@gmail.com>
2024-03-16 06:15:12 +0300

    AudioManager and AudioTest Created
    check audiotest class for example codes

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    //how to use AudioManager guide
+    void Start()
+    {
+        AudioManager.instance.PlaySFX("CheckPoint");
+        AudioManager.instance.StartMusicAfterSecs(3);
+        StartCoroutine(InTimeTestCor(5));
+    }
+    private IEnumerator InTimeTestCor(float time)
+    {
+        yield return new WaitForSeconds(time);
+        AudioManager.instance.musicSource.volume = 0.2f;
+        yield return new WaitForSeconds(time);
+        AudioManager.instance.musicSource.Stop();
+        AudioManager.instance.PlaySFX("Explosion");
+        AudioManager.instance.StartMusicAfterSecs(3);
+        
+        
+    }
+}
